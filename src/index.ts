e7daec2cd1a7e1761a6983d91ca8f6ef4d9bcb63
Varunkumar8055 <149817216+Varunkumar8055@users.noreply.github.com>
2025-03-24 11:29:07 +0530

    Implement JWT token generation with payload in the server setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import jwt from "jsonwebtoken";
 
-const app = new Hono()
+const payload : jwt.JwtPayload = {
+  iss : "https://msvarunkumar504@gmail.com",
+  sub : "Varunkumar8055",
+};
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const secretkey = "HelloWorld";
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+const token = jwt.sign(payload, secretkey);
